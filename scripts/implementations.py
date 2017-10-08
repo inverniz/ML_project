561a33faa14faab180c99e62bdb098f041b3463d
Invernizzi Hakim <hakim.invernizzi@epfl.ch>
2017-10-08 14:29:40 +0200

    added template for project

---
+++ scripts/implementations.py
@@ -0,0 +1,50 @@
+
+"""some machine learning methods for project 1."""
+import numpy as np
+
+"""Linear regression using gradient descent."""
+def least squares GD(y, tx, initial w, max iters, gamma):
+    w = 0
+    loss = 0
+    
+    return w, loss
+
+
+"""Linear regression using stochastic gradient descent."""
+def least squares SGD(y, tx, initial w, max iters, gamma):
+    w = 0
+    loss = 0
+    
+    return w, loss
+
+
+"""Least squares regression using normal equations."""
+def least squares(y, tx):
+    w = 0
+    loss = 0
+    
+    return w, loss
+
+
+"""Ridge regression using normal equations."""
+def ridge regression(y, tx, lambda ):
+    w = 0
+    loss = 0
+    
+    return w, loss
+
+
+"""Logistic regression using gradient descent or SGD."""
+def logistic regression(y, tx, initial w, max iters, gamma):
+    w = 0
+    loss = 0
+    
+    return w, loss
+
+
+"""Regularized logistic regression using gradient descent or SGD."""
+def reg logistic regression(y, tx, lambda , initial w, max iters, gamma):
+    w = 0
+    loss = 0
+    
+    return w, loss
