6cee727ea427609ba5e36afd76919ce8a7d7a167
Invernizzi Hakim <hakim.invernizzi@epfl.ch>
2017-10-12 20:45:18 +0200

    tentative implementation of GD and SGD

--- scripts/implementations.py
+++ scripts/implementations.py
@@ -1,25 +1,69 @@
-
 """some machine learning methods for project 1."""
 import numpy as np
 
+"""Compute the gradient for gradient descent."""
+def compute_gradient_GD(y, tx, w):
+	error = y - tx.dot(w)
+	n = y.shape[0]
+	x_transposed = tx.T
+	gradient = -x_transposed.dot(error)/n
+	
+	return gradient
+
+"""Compute the gradient for stochastic gradient descent."""
+def compute_gradient_SGD(y, tx, w, sample_index):
+    yn = y[sample_index]
+    xnt = tx[sample_index].T
+    error = np.power(yn - xnt.dot(w), 2)
+    gradient = -xnt.dot(error)
+    
+    return gradient
+
+"""Calculate the loss for GD."""
+def compute_loss_GD(y, tx, w):
+    error = y - tx.dot(w)
+    n = y.shape[0]
+    loss = error.T.dot(error)/(2*n)
+    
+    return loss
+
+"""Calculate the partial loss for SGD."""
+def compute_loss_SGD(y, tx, w, sample_index):
+    yn = y[sample_index]
+    xnt = tx[sample_index].T
+    error = np.power(yn - xnt.dot(w), 2)
+    loss =  1/2*error
+    
+    return loss
+
 """Linear regression using gradient descent."""
-def least squares GD(y, tx, initial w, max iters, gamma):
-    w = 0
-    loss = 0
+def least_squares_GD(y, tx, initial_w, max_iters, gamma):
+    w = initial_w
+    for n_iter in range(max_iters):
+        gradient = compute_gradient_GD(y, tx, w)
+        loss = compute_loss_GD(y, tx, w)
+        w = w - gamma*gradient
     
     return w, loss
 
-
 """Linear regression using stochastic gradient descent."""
-def least squares SGD(y, tx, initial w, max iters, gamma):
-    w = 0
-    loss = 0
-    
+def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
+    w = initial_w
+    n = y.shape[0]
+    for n_iter in range(max_iters):
+        loss = 0
+        for sample_index in range(n):
+            gradient = compute_gradient_SGD(y, tx, w, sample_index)
+            partial_loss = compute_loss_SGD(y, tx, w, sample_index)
+            loss += partial_loss
+            w = w - gamma*gradient
+
+    loss/= n
     return w, loss
 
 
 """Least squares regression using normal equations."""
-def least squares(y, tx):
+def least_squares(y, tx):
     w = 0
     loss = 0
     
@@ -27,7 +71,7 @@ def least squares(y, tx):
 
 
 """Ridge regression using normal equations."""
-def ridge regression(y, tx, lambda ):
+def ridge_regression(y, tx, lambda_ ):
     w = 0
     loss = 0
     
@@ -35,7 +79,7 @@ def ridge regression(y, tx, lambda ):
 
 
 """Logistic regression using gradient descent or SGD."""
-def logistic regression(y, tx, initial w, max iters, gamma):
+def logistic_regression(y, tx, initial_w, max_iters, gamma):
     w = 0
     loss = 0
     
@@ -43,7 +87,7 @@ def logistic regression(y, tx, initial w, max iters, gamma):
 
 
 """Regularized logistic regression using gradient descent or SGD."""
-def reg logistic regression(y, tx, lambda , initial w, max iters, gamma):
+def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
     w = 0
     loss = 0
     
